fcbe58cd9bdd9b8f4e867d08158cfb329530348c
pablo258-s <82181944+pablo258-s@users.noreply.github.com>
2021-05-25 13:00:47 -0600

    Add files via upload

---
+++ pablocamey5tobach,prograComercial/src/main/index.java
@@ -0,0 +1,11 @@
+
+package main;
+import objetos.ventanaUno;
+public class index {
+    public static void main (String args[]){
+     
+        ventanaUno b1 = new ventanaUno();
+        b1.setVisible(true);
+        b1.setLocationRelativeTo(null);
+}
+}
